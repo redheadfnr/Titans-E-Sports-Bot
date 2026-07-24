319bc412f63a872d369c1f020a018b8e62b30427
redheadfnr <redhead0076@gmail.com>
2026-07-25 01:21:56 +0200

    Increase base win chance from 0.4 to 0.5

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -4,7 +4,7 @@ import { getEconomyData, setEconomyData } from '../../utils/economy.js';
 import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const BASE_WIN_CHANCE = 0.4;
+const BASE_WIN_CHANCE = 0.5;
 const CLOVER_WIN_BONUS = 0.1;
 const CHARM_WIN_BONUS = 0.08;
 const PAYOUT_MULTIPLIER = 2.0;
